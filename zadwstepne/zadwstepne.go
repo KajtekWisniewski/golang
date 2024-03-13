9c9138f3c470e6d9cb26fe654b3e61ca5eabbeb1
LECFanboy <64749090+LECFanboy@users.noreply.github.com>
2024-03-13 19:21:20 +0100

    zad montyhall

--- zadwstepne/zadwstepne.go
+++ zadwstepne/zadwstepne.go
@@ -1,26 +1,80 @@
 package main
 
-import "fmt"
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
+func runTrial(switchBoxes bool, nBoxes int) bool {
 
-func main() {
+	chosenDoor := rand.Intn(nBoxes) + 1
 
-	no_rounds := 10
-	var player_strat bool
-	var winning_rounds int32
+	if switchBoxes {
+		revealedDoor := 3
+		if chosenDoor == 2 {
+			revealedDoor = 3
+		} else {
+			revealedDoor = 2
+		}
 
-	for i:=0; i < no_rounds; i++ {
+		var availableDoors []int
+		for dnum := 1; dnum <= nBoxes; dnum++ {
+			if dnum != chosenDoor && dnum != revealedDoor {
+				availableDoors = append(availableDoors, dnum)
+			}
+		}
 
-		winning_number:=rand.Intn(3)
-		player_choice:=rand.Intn(3)
-		choices := make([]bool, 3)
-		choices[winning_number] = true
-		if player_strat == false && choices[player_choice] == true {
-			winning_rounds++
+		chosenDoor = availableDoors[rand.Intn(len(availableDoors))]
+	}
+
+	return chosenDoor == 1
+}
+
+func runTrials(nTrials int, switchDoors bool, nDoors int) int {
+	nWins := 0
+
+	for i := 0; i < nTrials; i++ {
+		if runTrial(switchDoors, nDoors) {
+			nWins++
 		}
 	}
 
-    fmt.Println("Hello, World!")
-	fmt.Println(winning_rounds)
+	return nWins
 }
+
+func main() {
+	nDoors, nTrials := 3, 10000
+	nWinsWithoutSwitch := runTrials(nTrials, false, nDoors)
+	nWinsWithSwitch := runTrials(nTrials, true, nDoors)
+
+	fmt.Printf("Monty Hall Problem with %d doors\n", nDoors)
+	fmt.Printf("Proportion of wins without switching: %.4f\n", float64(nWinsWithoutSwitch)/float64(nTrials))
+	fmt.Printf("Proportion of wins with switching: %.4f\n", float64(nWinsWithSwitch)/float64(nTrials))
+}
+
+// package main
+
+// import "fmt"
+// import "math/rand"
+
+
+// func main() {
+
+// 	no_rounds := 10
+// 	var player_strat bool
+// 	var winning_rounds int32
+
+// 	for i:=0; i < no_rounds; i++ {
+
+// 		winning_number:=rand.Intn(3)
+// 		player_choice:=rand.Intn(3)
+// 		choices := make([]bool, 3)
+// 		choices[winning_number] = true
+// 		if player_strat == false && choices[player_choice] == true {
+// 			winning_rounds++
+// 		}
+// 	}
+
+//     fmt.Println("Hello, World!")
+// 	fmt.Println(winning_rounds)
+// }
